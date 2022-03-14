92939b700b40eb95111db9c2aebb60fe4479736f
timlant <tim.lant@asu.edu>
2022-03-14 13:06:40 -0700

    loading more text

--- listing_1_1.R
+++ listing_1_1.R
@@ -1,1 +1,14 @@
-## 
+## initial event that starts the cascade (i.e. the immigrant)
+## mark = 1000 at time t=0
+event <- c(mark = 1000, time = 0)
+## the timepoints for which we compute the kernel function
+t <- seq(from = 0, to = 100, length = 1000)
+## set the parameters of the kernel
+K <- 0.8
+beta <- 0.6
+c <- 10
+theta <- 0.8
+
+## compute the Power Law Kernel
+## call the kernelFunction to get the values
+values.PL <- kernelFct(event = event, t = t, K = K, beta = beta, c = c, theta = theta, kernel.type='PL')
